3815ab0c79ac595453abb25c61aa4696f823222f
beksultanovcl <beksultanovcl7@gmail.com>
2026-01-29 12:45:35 +0600

    изменение

--- papka/lesson.py
+++ papka/lesson.py
@@ -1,5 +1,14 @@
 class Lesson:
-    def __init__(self, name):
+    def __init__(self, name, age, heigh):
         self.name = name
+        self.age = age
+        self.heigh = heigh
     def info(self):
         return f'{self.name} info'
+
+    def height(self):
+        return f'Рост {self.name} состовляет {self.heigh}'
+
+a = Lesson('bakbergen', 18, 1.75)
+print(a.height())
+
